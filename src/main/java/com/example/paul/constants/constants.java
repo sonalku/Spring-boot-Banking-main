8e28c98ccf65d740c344f17443ac8777b5ec5d7a
sonal786 <ksonalkumbhare@gmail.com>
2023-04-08 19:00:33 +0300

    second

--- src/main/java/com/example/paul/constants/constants.java
+++ src/main/java/com/example/paul/constants/constants.java
@@ -1,5 +1,8 @@
 package com.example.paul.constants;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class constants {
@@ -24,4 +27,39 @@ public class constants {
             "Account information is invalid or transaction has been denied for your protection. Please try again.";
     public static final String CREATE_ACCOUNT_FAILED =
             "Error happened during creating new account";
+    
+    /********************************************/
+    
+    public static final String BANKING = "BANKING"; 
+    public static final String OTHER_SERVICES = "OTHER SERVICES"; 
+    public static final String STATEMENT = "STATEMENT"; 
+    public static final String CARDS = "CARDS"; 
+    public static final String ACCOUNT = "ACCOUNT";
+    
+    public static final String SHOW_TRANSACTION = "SHOW TRANSACTION";
+	public static final String CHECK_BALANCE = "CHECK BALANCE";
+	public static final String LAST_TRANSACTION = "LAST TRANSACTION";
+	public static final String CARD_LIMIT = "CARD LIMIT";
+	public static final String PENDING_BILL = "PENDING BILL";
+	public static final String SEND_MONEY = "SEND MONEY";
+	
+	public static final String CHANGE_ADDRESS = "CHANGE ADDRESS";
+	public static final String CHECK_STATUS = "CHECK STATUS";
+	public static final String REQUEST_CHECK_BOOK = "REQUEST CHECKBOOK";
+	public static final String BLOCK_CARD = "BLOCK CARD";
+	public static final String CHANGE_PIN = "CHANGE PIN";
+	
+	public static final String SHOW_BANALCE = "SHOW BANALCE";
+	
+	public static final String GET_STATEMENT = "GET STATEMENT";
+	
+	public static final String WRONG_INPUT_OPTIONS_MESSAGE = "PLEASE INPUT CORRECT CHOICE FROM GIVEN OPTIONS";
+    
+    public static final List<String> OPTIONS = new ArrayList<String>(Arrays.asList(BANKING,OTHER_SERVICES,STATEMENT,CARDS,ACCOUNT));
+    public static final List<String> BANKING_OPTIONS = new ArrayList<String>(Arrays.asList(SHOW_TRANSACTION,CHECK_BALANCE,LAST_TRANSACTION,SEND_MONEY));
+    public static final List<String> OTHER_SERVICES_OPTIONS = new ArrayList<String>(Arrays.asList(CHANGE_ADDRESS,CHECK_STATUS,REQUEST_CHECK_BOOK,BLOCK_CARD,CHANGE_PIN));
+    public static final List<String> STATEMENTS_OPTIONS = new ArrayList<String>(Arrays.asList(GET_STATEMENT));
+    public static final List<String> CARDS_OPTIONS = new ArrayList<String>(Arrays.asList(CARD_LIMIT,PENDING_BILL));
+    public static final List<String> ACCOUNT_OPTIONS = new ArrayList<String>(Arrays.asList(SHOW_BANALCE));
+    public static final List<String> WRONG_INPUT_OPTIONS = new ArrayList<String>(Arrays.asList(WRONG_INPUT_OPTIONS_MESSAGE));
 }
---
+++ src/main/java/com/example/paul/controllers/BankingController.java
@@ -0,0 +1,45 @@
+package com.example.paul.controllers;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.paul.constants.constants;
+
+@RestController
+@RequestMapping("api/v1/bank")
+public class BankingController {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(BankingController.class);
+	
+	@GetMapping()
+    public ResponseEntity<List<String>> getOptions() {
+        LOGGER.debug("Triggered AccountRestController.accountInput");     
+		return new ResponseEntity<List<String>>(constants.OPTIONS,HttpStatus.OK);
+    }
+	
+	@GetMapping(value = "/options/{inputValue}")
+    public ResponseEntity<List<String>> getOptions(
+    		@RequestParam(name = "inputValue", required = true) String inputValue
+    		) {
+        LOGGER.debug("Triggered AccountRestController.accountInput");  
+        switch (inputValue) {
+		case constants.BANKING : return new ResponseEntity<List<String>>(constants.BANKING_OPTIONS,HttpStatus.OK);
+		case constants.OTHER_SERVICES : return new ResponseEntity<List<String>>(constants.OTHER_SERVICES_OPTIONS,HttpStatus.OK);
+		case constants.STATEMENT : return new ResponseEntity<List<String>>(constants.STATEMENTS_OPTIONS,HttpStatus.OK);
+		case constants.CARDS : return new ResponseEntity<List<String>>(constants.CARDS_OPTIONS,HttpStatus.OK);
+		case constants.ACCOUNT : return new ResponseEntity<List<String>>(constants.ACCOUNT_OPTIONS,HttpStatus.OK);
+		default:
+			return new ResponseEntity<List<String>>(constants.WRONG_INPUT_OPTIONS,HttpStatus.OK);
+		}
+		
+    }
+
+}
