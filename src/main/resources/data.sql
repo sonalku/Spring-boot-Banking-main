4e93e9a7c650010dfb1c6010e4643600c260f176
bamnev786 <bamnev786@gmail.com>
2023-04-10 17:45:28 +0300

    adjusted DB data values

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,8 +2,6 @@ INSERT INTO online_bank.account (id,account_type, ifsc_code, account_number, cur
 VALUES (1,'SAVINGS', '53-68-92', '73084635', 15000000, 'Easy Bank', 'John Lewis', 'I AM FINE');
 INSERT INTO online_bank.account (id,account_type, ifsc_code, account_number, current_balance, bank_name, owner_name, security_code)
 VALUES (2,'CURRENT', '65-93-37', '21956204', 250000000, 'Easy Bank', 'John Lewis', 'DO IT NOW');
-INSERT INTO online_bank.account (id,account_type, ifsc_code, account_number, current_balance, bank_name, owner_name, security_code)
-
 
 
 INSERT INTO online_bank.transaction (id, account_number, beneficiary_name, amount, transaction_date )
@@ -29,7 +27,7 @@ VALUES (10, '21956204', 'John Lewis', 90780, '2019-08-01 10:30');
 INSERT INTO online_bank.transaction (id, account_number, beneficiary_name, amount, transaction_date )
 VALUES (11, '21956204', 'John Lewis', 75400, '2019-08-01 10:30');
 INSERT INTO online_bank.transaction (id, account_number, beneficiary_name, amount, transaction_date )
-VALUES (12, '73084635', 'John Lewis', 1000000, '2019-08-27 17:21');
+VALUES (12, '73084635', 'John Lewis', 100000, '2019-08-27 17:21');
 
 INSERT INTO online_bank.cards (id, account_number, card_Type, card_Number, current_Balance, billing_Date, card_Limit)
 VALUES (1, '73084635', 'CREDIT', '1111-2222-3333-2323', 200000, '2019-05-01 00:00', 500000);
